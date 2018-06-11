6c5656177818c5134770b37ba3ff20dbd95affaf
anotoniomusolino <antoniomusolino007@gmail.com>
2018-06-11 18:55:30 +0200

    i do know

--- command_parser.c
+++ command_parser.c
@@ -41,10 +41,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 #define _POSIX_SOURCE 1
-#include <string.h>
-
 /*
  * strtok_r documentation:
  * http://pubs.opengroup.org/onlinepubs/009695399/functions/strtok.html
